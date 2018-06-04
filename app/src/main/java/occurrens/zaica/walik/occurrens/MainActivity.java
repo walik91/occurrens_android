a5e305dedc74733c54e54dc68655fd903ecb7a48
Valentin <walik91@gmail.com>
2018-06-04 18:28:09 +0300

    Update MainActivity.java

--- app/src/main/java/occurrens/zaica/walik/occurrens/MainActivity.java
+++ app/src/main/java/occurrens/zaica/walik/occurrens/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("walik", "test_change");
+        Log.d("walik", "test_change 2");
     }
 }
